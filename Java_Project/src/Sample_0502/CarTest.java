b4d680c2bb220b479a2baa832cfb38ea8123b641
Java1-2014024010 <pdzzo@nate.com>
2016-05-02 20:12:21 +0900

    Bye!

---
+++ Java_Project/src/Sample_0502/CarTest.java
@@ -0,0 +1,18 @@
+package Sample_0502;
+
+public class CarTest {
+
+	//static Car c1 = new Car ("1","2",3);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Car.numbers);
+		
+		//Car c1 =  new Car("Prius","Light Gray",200);
+		//c1.
+		System.out.println(Car.numbers);
+		
+		Car.Car_Number();
+		
+	}
+
+}
